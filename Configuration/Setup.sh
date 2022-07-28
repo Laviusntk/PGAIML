eae1856e1ead2efabaf528d94502ed5f45d2afa9
Lavius Motileng <103624603+Laviusntk@users.noreply.github.com>
2022-07-28 16:43:34 -0700

    Update Setup.sh

--- Configuration/Setup.sh
+++ Configuration/Setup.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Laviusntk/Configurations.git
 cd Configurations/Configurations/Anaconda
 
 chmod 755 ./SetupCondaAIEnvironment.sh
-sh ./SetupCondaAIEnvironment.sh
+bash ./SetupCondaAIEnvironment.sh
